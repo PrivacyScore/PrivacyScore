a8d3e3ea61b8cd662685b4ecfe4027f7a552a1ca
Max Maass <mmaass@seemoo.tu-darmstadt.de>
2017-03-21 11:49:22 +0100

    Fix SQL Injections in db_connector.py

--- scanner/scanner/db_connector.py
+++ scanner/scanner/db_connector.py
@@ -74,11 +74,10 @@ class DBConnector():
                 site = sites.next()
 
                 # requests
-                # FIXME SQL Injection
                 for start_time, site_url in cur.execute(
                         "SELECT DISTINCT start_time, site_url " +
                         "FROM crawl as c JOIN site_visits as s " +
-                        "ON c.crawl_id = s.crawl_id WHERE site_url LIKE '" + site["url"] + "';"):
+                        "ON c.crawl_id = s.crawl_id WHERE site_url LIKE ?;", (site["url"],)):
                     scantosave["starttime"] = start_time
                     scantosave["scan_group_id"] = ObjectId(scangroup_id)
                     scantosave["site_id"] = ObjectId(site["_id"])
@@ -90,10 +89,9 @@ class DBConnector():
                     maindomain_visited_url = "{}.{}".format(extracted_visited_url.domain, extracted_visited_url.suffix)
                     hostname_visited_url = '.'.join(extracted_visited_url)
 
-                    # FIXME SQL Injection
                     for url, method, referrer, headers in cur.execute("SELECT url, method, referrer, headers " +
                             "FROM site_visits as s JOIN http_requests as h ON s.visit_id = h.visit_id " +
-                            "WHERE s.site_url LIKE '" + site["url"] + "' ORDER BY h.id;"):
+                            "WHERE s.site_url LIKE ? ORDER BY h.id;", (site["url"],)):
                         request = {
                             'url': url,
                             'method': method,
@@ -120,11 +118,10 @@ class DBConnector():
                     scantosave["third_parties_anzahl"] = len(third_parties)
 
                     # responses
-                    # FIXME SQL Injection
                     for url, method, referrer, headers, response_status_text, time_stamp in cur.execute(
                             "SELECT url, method, referrer, headers, response_status_text, " +
                             "time_stamp FROM site_visits as s JOIN http_responses as h " +
-                            "ON s.visit_id = h.visit_id WHERE s.site_url LIKE '" + site["url"] + "' ORDER BY h.id;"):
+                            "ON s.visit_id = h.visit_id WHERE s.site_url LIKE ? ORDER BY h.id;", (site["url"],)):
                         response = {
                             'url': url,
                             'method': method,
@@ -224,12 +221,11 @@ class DBConnector():
 
 
                     # Cookies
-                    # FIXME SQL Injection
                     for baseDomain, name, value, host, path, expiry, accessed, creationTime, isSecure, isHttpOnly in cur.execute(
                             "SELECT baseDomain, name, value, host, path, expiry, " +
                             "accessed, creationTime, isSecure, isHttpOnly " +
                             "FROM site_visits as s JOIN profile_cookies as c " +
-                            "ON s.visit_id = c.visit_id WHERE s.site_url LIKE '" + site["url"] + "';"):
+                            "ON s.visit_id = c.visit_id WHERE s.site_url LIKE ?;", (site["url"],)):
                         profilecookie = {
                             'baseDomain': baseDomain,
                             'name': name,
@@ -245,11 +241,10 @@ class DBConnector():
                         scantosave["profilecookies"].append(profilecookie)
 
                     # Flash-Cookies
-                    # FIXME SQL Injection
                     for domain, filename, local_path, key, content in cur.execute(
                             "SELECT domain, filename, local_path, key, content " +
                             "FROM site_visits as s JOIN flash_cookies as c " +
-                            "ON s.visit_id = c.visit_id WHERE s.site_url LIKE '" + site["url"] + "';"):
+                            "ON s.visit_id = c.visit_id WHERE s.site_url LIKE ?;", (site["url"],)):
                         flashcookie = {
                             'domain': domain,
                             'filename': filename,
