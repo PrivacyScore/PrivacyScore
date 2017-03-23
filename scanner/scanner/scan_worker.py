6031efd050d5326ddebd6759e05c17e12c6b4394
Max Maass <mmaass@seemoo.tu-darmstadt.de>
2017-03-23 10:06:53 +0100

    Push scans into their own functions

--- scanner/scanner/scan_worker.py
+++ scanner/scanner/scan_worker.py
@@ -12,6 +12,49 @@ from externaltests_connector import ExternalTestsConnector
 
 import config
 
+def scan_openwpm(urls, list_id, scangroup_id):
+    state = ScannerConnector.startscan(urls, list_id, scangroup_id)
+    if state.startswith('error'):
+        # Something went wrong, update the database with infos
+        # Get DB connection
+        # TODO MongoDB access - refactor out in the long run
+        client = MongoClient(config.MONGODB_URL)
+        db = client['PrangerDB']
+        # TODO And this will obviously be overwritten immediately by the other scan scripts...
+        db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'state': "error in ScannerConnector.startscan - %s" % state}})
+        print "error in ScannerConnector.startscan - %s" % state
+        # Need a good way to handle the error here
+        # Maybe return False and have the Chord processor deal with it? But what can it do? Nothing
+
+def scan_external(urls, list_id, scangroup_id):
+    state = ExternalTestsConnector.RunExternalTests(list_id, scangroup_id, urls)
+    if state.startswith('error'):
+        # TODO MongoDB access - refactor out in the long run
+        client = MongoClient(config.MONGODB_URL)
+        db = client['PrangerDB']
+        db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'state': "error during RunExternalTests - %s" % state}})
+        print "error during RunExternalTests - %s" % state
+        # TODO Handle this better
+
+def save_to_database(urls, list_id, scangroup_id):
+    state = DBConnector.SaveScan(list_id, scangroup_id, urls)
+    # TODO The following is just error handling for the insert - will probably also have to be moved (statekeeping in MongoDB)
+    client = MongoClient(config.MONGODB_URL)
+    db = client['PrangerDB']
+    if state.startswith('error'):
+        db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'state': "error during SaveScan - %s" % state}})
+        print "error during SaveScan - %s" % state
+
+    elif state.startswith('success'):
+        db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'state': 'finish'}})
+        db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'progress': "finish"}})
+        db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set':{'progress_timestamp': datetime.now().isoformat()}}, upsert=False)
+
+    else:
+        db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'state': 'unknown error during SaveScan: no status returned'}})
+        print "unknown error during SaveScan: no status returned"
+
+
 DBConnector = DBConnector()
 ScannerConnector = ScannerConnector()
 ExternalTestsConnector = ExternalTestsConnector()
@@ -31,8 +74,11 @@ if scanninglists < 3 and readylists > 0:
     if len(x) > 0:
         for list in x:
             urls = []
-            list_id = list['list_id']
-            scangroup_id = list['_id']
+            # list_id and _id are bson.ObjectId's, originally. We cast them to string because ObjectId's cannot be serialized,
+            # which is important for Celery. They can be converted back into ObjectId's by a simple ObjectId(list_id) without loss
+            # of functionality, it seems
+            list_id = str(list['list_id'])
+            scangroup_id = str(list['_id'])
 
             # Note: This is a race condition waiting to happen if a scan has not terminated before a rescan is started.
             # But I don't have a better idea right now.
@@ -57,32 +103,12 @@ if scanninglists < 3 and readylists > 0:
 
             # Scan using OpenWTM
             # TODO Move to Celery task
-            state = ScannerConnector.startscan(urls, list_id, scangroup_id)
-            if state.startswith('error'):
-                db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'state': "error in ScannerConnector.startscan - %s" % state}})
-                sys.exit("error in ScannerConnector.startscan - %s" % state)
+            scan_openwpm(urls, list_id, scangroup_id)
 
             # Run external tests
             # TODO Move to Celery task
-            state = ExternalTestsConnector.RunExternalTests(list_id, scangroup_id, urls)
-            if state.startswith('error'):
-                db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'state': "error during RunExternalTests - %s" % state}})
-                sys.exit("error during RunExternalTests - %s" % state)
+            scan_external(urls, list_id, scangroup_id)
 
             # Save results to MongoDB
             # TODO This will have to be moved to a different place in the long run, once we use Celery tasks
-            state = DBConnector.SaveScan(list_id, scangroup_id, urls)
-            # TODO The following is just error handling for the insert - will probably also have to be moved (statekeeping in MongoDB)
-            if state.startswith('error'):
-                db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'state': "error during SaveScan - %s" % state}})
-                sys.exit("error during SaveScan - %s" % state)
-
-            elif state.startswith('success'):
-                db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'state': 'finish'}})
-                db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'progress': "finish"}})
-                db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set':{'progress_timestamp': datetime.now().isoformat()}}, upsert=False)
-                sys.exit()
-
-            else:
-                db.ScanGroup.update({'_id': ObjectId(scangroup_id)}, {'$set': {'state': 'unknown error during SaveScan: no status returned'}})
-                sys.exit("unknown error during SaveScan: no status returned")
+            save_to_database(urls, list_id, scangroup_id)
