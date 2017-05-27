8cb2d1a425a6a4b5231a2ae612c0adbba62ac644
Pascal Wichmann <git@pa-w.de>
2017-05-27 10:31:23 +0200

    Add classification to descriptions

--- privacyscore/evaluation/description.py
+++ privacyscore/evaluation/description.py
@@ -11,38 +11,41 @@ from privacyscore.evaluation.result_groups import RESULT_GROUPS
 def describe_locations(server_type: str, locations: list) -> str:
     """Describe a list of locations."""
     if not locations or locations == ['']:
-        return _('The location of the %(server_type)s could not '
-                 'be detected.') % {'server_type': server_type}
+        return _('The locations of the %(server_type)s could not '
+                 'be detected.') % {'server_type': server_type}, 'bad'
     if len(locations) == 1:
         return _('All %(server_type)s are located in %(country)s.') % {
             'server_type': server_type,
             'country': locations[0]
-        }
+        }, 'neutral'
     return _('The %(server_type)s are located in %(countries)s.') % {
         'server_type': server_type,
         'countries': ', '.join(locations[:-1]) + ' and {}'.format(locations[-1])
-    }
+    }, 'neutral'
 
 
 # The mapping specifies a function for each key to create a description
-# explaining the result to a user.
+# explaining the result to a user and a classification.
 # TODO: Cleaner solution? Inline lambdas are ugly and not flexible at all.
+# TODO: More flexible classifications -- class objects?
+# TODO: classification partly redundant with evaluation (though descriptions 
+#       can describe multiple attributes)
 MAPPING = {
     'general': [
         (('cookies_count',),
-            lambda v: _('The site is not using cookie.') if v[0] == 0 \
-                else _('The site is using %(count)d cookies.') % {
-                    'count': v[0]}),
+            lambda v: (_('The site is not using cookies.'), 'good') if v[0] == 0 \
+                else (_('The site is using %(count)d cookies.') % {
+                    'count': v[0]}, 'bad')),
         (('flashcookies_count',),
-            lambda v: _('The site is not using flash cookie.') if v[0] == 0 \
-                else _('The site is using %(count)d flash cookies.') % {
-                    'count': v[0]}),
+            lambda v: (_('The site is not using flash cookies.'), 'good') if v[0] == 0 \
+                else (_('The site is using %(count)d flash cookies.') % {
+                    'count': v[0]}, 'bad')),
         (('third_parties_count',),
-            lambda v: _('The site does not use any third parties.') if v[0] == 0 \
-                else _('The site is using %(count)d different third parties.') % {
-                    'count': v[0]}),
+            lambda v: (_('The site does not use any third parties.'), 'good') if v[0] == 0 \
+                else (_('The site is using %(count)d different third parties.') % {
+                    'count': v[0]}, 'bad')),
         (('leaks',),
-            lambda v: _('The site discloses internal system information that should not be available.') if v[0] == 0 else None),
+            lambda v: (_('The site discloses internal system information that should not be available.'), 'bad') if v[0] == 0 else None),
     ],
     'privacy': [
         (('a_locations',), lambda v: describe_locations(_('web servers'), v[0])),
@@ -50,21 +53,21 @@ MAPPING = {
     ],
     'ssl': [
         (('pfs',),
-            lambda v: _('The server is supporting perfect forward secrecy.') if v[0]\
-                else _('The site is not supporting perfect forward secrecy.'),),
+            lambda v: (_('The server is supporting perfect forward secrecy.'), 'good') if v[0]\
+                else (_('The site is not supporting perfect forward secrecy.'), 'bad')),
         (('has_hsts_header',),
         # TODO: header validity, inclusion in upstream preload list etc.
-            lambda v: _('The server uses HSTS to prevent insecure requests.') if v[0] \
-                else _('The site is not using HSTS to prevent insecure requests.'),),
+            lambda v: (_('The server uses HSTS to prevent insecure requests.'), 'good') if v[0] \
+                else (_('The site is not using HSTS to prevent insecure requests.'), 'bad')),
         (('has_hpkp_header',),
-            lambda v: _('The server uses Public Key Pinning to prevent attackers to use invalid certificates.') if v[0] \
-                else _('The site is not using Public Key Pinning to prevent attackers to use invalid certificates.'),),
+            lambda v: (_('The server uses Public Key Pinning to prevent attackers to use invalid certificates.'), 'good') if v[0] \
+                else (_('The site is not using Public Key Pinning to prevent attackers to use invalid certificates.'), 'bad')),
         (('has_protocol_sslv2', 'has_protocol_sslv3'),
-            lambda v: _('The server supports insecure protocols.') if any(v) \
-                else _('The server does not support insecure protocols.'),),
+            lambda v: (_('The server supports insecure protocols.'), 'bad') if any(v) \
+                else (_('The server does not support insecure protocols.'), 'good')),
         (('has_protocol_tls1', 'has_protocol_tls1_1', 'has_protocol_tls1_2'),
-            lambda v: _('The server supports secure protocols.') if any(v) \
-                else _('The server does not support secure protocols.'),),
+            lambda v: (_('The server supports secure protocols.'), 'good') if any(v) \
+                else (_('The server does not support secure protocols.'), 'bad')),
     ],
 }
 
