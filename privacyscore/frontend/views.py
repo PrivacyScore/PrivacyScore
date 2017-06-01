969955f5db2f5c7ac8c515c5fbf6c384535eb5fb
Pascal Wichmann <git@pa-w.de>
2017-06-01 22:35:34 +0200

    Add RESULT_GROUPS entries one-by-one to preserve ordering in older python versions; fix incorrect hard-coded group ordering

--- privacyscore/frontend/views.py
+++ privacyscore/frontend/views.py
@@ -108,7 +108,7 @@ def view_scan_list(request: HttpRequest, scan_list_id: int) -> HttpResponse:
         result = site.last_scan.result_or_none
         if not result:
             continue
-        site.evaluated = result.evaluate(RESULT_GROUPS, ['general', 'ssl', 'privacy'])[0]
+        site.evaluated = result.evaluate(RESULT_GROUPS, ['general', 'privacy', 'ssl'])[0]
 
     sites = sorted(sites, key=lambda v: v.evaluated, reverse=True)
 
