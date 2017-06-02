f8f6e1502b8a8820604119b9e1c2ca5a9dea4a4b
Pascal Wichmann <git@pa-w.de>
2017-06-02 21:34:06 +0200

    Undetectable (mail) server locations are neutral

--- privacyscore/evaluation/description.py
+++ privacyscore/evaluation/description.py
@@ -1,20 +1,16 @@
 """
 This module defines textual representations/explanations for results of keys.
 """
-from typing import Iterable, Tuple
-
 from django.utils.translation import ugettext_lazy as _
-from django.utils.translation import ungettext_lazy
 
 from privacyscore.evaluation.rating import Rating
-from privacyscore.evaluation.result_groups import RESULT_GROUPS
 
 
 def describe_locations(server_type: str, locations: list) -> str:
     """Describe a list of locations."""
     if not locations or locations == ['']:
         return _('The locations of the %(server_type)s could not '
-                 'be detected.') % {'server_type': server_type}, Rating('bad', influences_ranking=False)
+                 'be detected.') % {'server_type': server_type}, Rating('neutral', influences_ranking=False)
     if len(locations) == 1:
         return _('All %(server_type)s are located in %(country)s.') % {
             'server_type': server_type,
