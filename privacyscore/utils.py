6fe32a9c738b16d024b03aeca7faf9e056136f9d
Pascal Wichmann <git@pa-w.de>
2017-06-03 10:10:49 +0200

    normalize_url: Remove empty username/password as well; store parsed url in variable to avoide parsing multiple times

--- privacyscore/utils.py
+++ privacyscore/utils.py
@@ -5,15 +5,16 @@ from url_normalize import url_normalize
 def normalize_url(url: str) -> str:
     """Normalize an url and remove GET query."""
     normalized = url_normalize(url)
-    explicit_port = urlparse(normalized).port
-    if explicit_port:
-        normalized = normalized.replace(':{}'.format(explicit_port), '', 1)
-    username = urlparse(normalized).username
-    password = urlparse(normalized).password
-    if username and password:
-        normalized = normalized.replace('{}:{}@'.format(username, password), '', 1)
-    elif username:
-        normalized = normalized.replace('{}@'.format(username), '', 1)
+    parsed = urlparse(normalized)
+    if parsed.port:
+        normalized = normalized.replace(
+            ':{}'.format(parsed.port), '', 1)
+    if parsed.username is not None and parsed.password is not None:
+        normalized = normalized.replace(
+            '{}:{}@'.format(parsed.username, parsed.password), '', 1)
+    elif parsed.username is not None:
+        normalized = normalized.replace(
+            '{}@'.format(parsed.username), '', 1)
     return normalized.split('?')[0]
 
 
