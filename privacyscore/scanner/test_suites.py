f426eb3428d51dd8787c9144065db8a9c1cd6da5
Pascal Wichmann <git@pa-w.de>
2017-06-04 10:59:20 +0200

    Scanner: Test suites: Catch exceptions during import to prevent whole scanner from failing if a single test suite is erroneous

--- privacyscore/scanner/test_suites.py
+++ privacyscore/scanner/test_suites.py
@@ -6,6 +6,7 @@ In addition, it generates the stages in which to run tests.
 """
 import os
 from importlib import import_module
+from sys import stderr
 
 from django.conf import settings
 from toposort import toposort
@@ -21,18 +22,32 @@ AVAILABLE_TEST_SUITES = {}
 
 # Load all modules
 for base_module in settings.TEST_SUITES_BASEMODULES:
-    base_module = import_module(base_module)
+    try:
+        base_module = import_module(base_module)
+    except Exception:
+        # base module can not be imported successfully.
+        print('Failure importing test suite base module {}'.format(base_module),
+              file=stderr)
+        continue
     for file in os.listdir(base_module.__path__[0]):
         if not file.endswith('.py') or '__' in file:
             continue
-        test_module = import_module('{}.{}'.format(
-            base_module.__name__, file[:-3]))
+        test_module = file[:-3]
+        try:
+            test_module = import_module('{}.{}'.format(
+                base_module.__name__, test_module))
+        except Exception:
+            print('Failure importing test suite {}'.format(test_module),
+                  file=stderr)
+            continue
         AVAILABLE_TEST_SUITES[test_module.test_name] = test_module
 
 
 # Generate stages based on dependencies.
 TEST_DEPENDENCIES = {}
 for test in (t[0] for t in settings.SCAN_TEST_SUITES):
+    if test not in AVAILABLE_TEST_SUITES:
+        continue
     TEST_DEPENDENCIES[test] = set(AVAILABLE_TEST_SUITES[test].test_dependencies)
 
 
