12f2dc587a0e7430e3f12da5dde1ec0a626f6753
Max Maass <mmaass@seemoo.tu-darmstadt.de>
2017-06-04 19:29:32 +0200

    Download HSTS Preloading lists

--- install-tests.sh
+++ install-tests.sh
@@ -44,3 +44,9 @@ mkdir tests/vendor/EasyList
 pushd tests/vendor/EasyList
 wget https://easylist.to/easylist/easyprivacy.txt https://easylist.to/easylist/easylist.txt https://easylist.to/easylist/fanboy-annoyance.txt
 popd
+
+# HSTS Preload List
+mkdir tests/vendor/HSTSPreload
+pushd tests/vendor/HSTSPreload
+wget https://chromium.googlesource.com/chromium/src/net/+/master/http/transport_security_state_static.json?format=TEXT -O - | base64 -d | egrep -v "^([ ]*\/\/|$)" > "transport_security_state_static"
+popd
