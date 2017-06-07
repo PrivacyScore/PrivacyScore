b57bb94f0ec07ace5d70d8426798ff2144a18551
Pascal Wichmann <git@pa-w.de>
2017-06-07 16:41:41 +0200

    Browse scans: Display relative scan times (closes #91)

---
+++ privacyscore/frontend/templatetags/arrow.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from typing import Union
+
+from arrow import Arrow
+from django import template
+
+from privacyscore.evaluation.result_groups import RESULT_GROUPS
+
+register = template.Library()
+
+
+@register.filter
+def humanize_datetime(value: Union[datetime,None]) -> str:
+    if value is None:
+        return 'never'
+    return Arrow.fromdatetime(value).humanize()
