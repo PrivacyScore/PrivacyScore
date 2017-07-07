db875fc8f2caa320a3902b585ebc638acdcf3cd7
Max Maass <mmaass@seemoo.tu-darmstadt.de>
2017-07-07 15:28:13 +0200

    Remove GPL headers again

--- privacyscore/evaluation/result_groups.py
+++ privacyscore/evaluation/result_groups.py
@@ -4,21 +4,6 @@ human-readable representation.
 
 At the moment, a default mapping of keys to groups is present. This should be
 replaced by a dynamic, user-defined group mapping should be usable.
-
-Copyright (C) 2017 PrivacyScore Contributors
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from collections import OrderedDict
 
