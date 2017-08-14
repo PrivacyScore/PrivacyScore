f4d3b65f8ae63d710dc0fb1bdc731071557a687e
Dominik Herrmann <dh@exomail.to>
2017-08-14 15:33:32 +0200

    Layout: extracted inline js in view_site.html to dedicated js file (preparing for CSP)

---
+++ privacyscore/frontend/static/frontend/js/view_site.js
@@ -0,0 +1,8 @@
+function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-menu-down glyphicon-menu-up');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
