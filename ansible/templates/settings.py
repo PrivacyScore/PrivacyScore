b1d1e06272250c241a982bc1aa2289e5487541b5
Tobias Mueller <muelli@cryptobitch.de>
2017-10-19 13:43:39 +0200

    ansible: Refactored SECRET_KEY into a variable

--- ansible/templates/settings.py
+++ ansible/templates/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '{{ lookup('passwordstore', 'privacyscore.org/settings/SECRET_KEY') }}'
+SECRET_KEY = "{{ privacyscore__secret_key | mandatory }}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = {{ privacyscore__debug | default(True) }}
