d56f46cc89eea457fa8cdbe03e7ff89fe3dab495
Tobias Mueller <muelli@cryptobitch.de>
2017-10-19 13:37:21 +0200

    ansible: Added a privacyscore__debug variable to control test environments
    We only want to install certain packages when we are in a test
    environment.  With this variable the dev requirements are only installed
    when we are indeed in debug mode.
    
    We currently default to True. We probably want to reconsider once the
    ansible stuff is working as expected, because we should aim for being
    secure by default.

--- ansible/templates/settings.py
+++ ansible/templates/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '{{ lookup('passwordstore', 'privacyscore.org/settings/SECRET_KEY') }}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = {{ privacyscore__debug | default(True) }}
 
 ALLOWED_HOSTS = {{ privacyscore__allowed_hosts | default([]) }}
 
