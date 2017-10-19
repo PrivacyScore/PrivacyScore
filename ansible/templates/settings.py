bbb167bbd2079eacb0f59604aafa95ac45ad39cd
Tobias Mueller <muelli@cryptobitch.de>
2017-10-19 13:51:15 +0200

    ansible: Add debug toolbar when in DEBUG mode

--- ansible/templates/settings.py
+++ ansible/templates/settings.py
@@ -143,3 +143,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/var/www/privacyscore.org'
+
+# debug toolbar
+if DEBUG:
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE = [
+        'debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
+    INTERNAL_IPS = ['127.0.0.1']
