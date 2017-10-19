22d0dc782e7de0e9a18a7d64a3d39a6fa134ce45
Tobias Mueller <muelli@cryptobitch.de>
2017-10-19 14:28:02 +0200

    ansible: soft-code collect-static behaviour
    We intend to run collect-static only if we're not in testing mode,
    because in testing mode we're probably serving through a runserver.

--- ansible/templates/settings.py
+++ ansible/templates/settings.py
@@ -152,7 +152,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/privacyscore.org'
+{% if privacyscore__collect_static %}
+STATIC_ROOT = '{{ privacyscore__static_root }}'
+{% endif %}
 
 # debug toolbar
 if DEBUG:
