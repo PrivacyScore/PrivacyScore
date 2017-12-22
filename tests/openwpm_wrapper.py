f46d927bca6340ec13705dd8a284e182cc941714
Dominik Herrmann <dh@exomail.to>
2017-12-22 17:43:00 +0100

    close manager before retrying

--- tests/openwpm_wrapper.py
+++ tests/openwpm_wrapper.py
@@ -183,6 +183,7 @@ def scan_site(site):
 
         if current_try+1 < max_tries:
             print("Trying again because scan failed...\n")
+            manager.close()
             save_screenshot = False
             shutil.rmtree(SCAN_DIR)
             os.mkdir(SCAN_DIR)
