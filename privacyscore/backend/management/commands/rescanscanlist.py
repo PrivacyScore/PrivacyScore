1c3a58ecefee176eaff1e0e0febe87892c12da75
Tobias Mueller <muelli@cryptobitch.de>
2018-01-10 12:42:18 +0100

    rescanscanlist: Time format the created field
    We don't need sub-second precision. In fact, I guess that the day would be sufficient.
    But we include the time for now so that we can remember better which list
    represents what.

--- privacyscore/backend/management/commands/rescanscanlist.py
+++ privacyscore/backend/management/commands/rescanscanlist.py
@@ -33,7 +33,7 @@ class Command(BaseCommand):
         scan_list_id = options['scan_list_id']
         if scan_list_id is None:
             for scan_list in ScanList.objects.all():
-                print ("{l.id:>8} \t {l.name} \t {l.created} \t {length}".format(
+                print ("{l.id:>8} \t {l.name} \t {l.created:%Y-%m-%d %H:%M} \t {length}".format(
                     l=scan_list,
                     length=len(scan_list.sites.all())))
                 print ()
