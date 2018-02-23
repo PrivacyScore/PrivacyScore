d6a0759b4e62d94f7b4f0f1782f7b0cd2c8a4b14
Max Maass <mmaass@seemoo.tu-darmstadt.de>
2018-02-23 16:14:05 +0100

    I should really learn this ORM

--- privacyscore/frontend/views.py
+++ privacyscore/frontend/views.py
@@ -600,7 +600,7 @@ def site_result_json(request: HttpRequest, site_id: int) -> HttpResponse:
             return render(request, 'frontend/site_result_json.html', {'site': site, 'highlighted_code': 'Incorrect timestamp format'})
         try:
             scan = Scan.objects.filter(site=site).filter(end__lte=request.GET['at']).order_by('-id').get()
-            scan_result = ScanResult.get(scan=scan)
+            scan_result = ScanResult.objects.get(scan=scan)
         except Exception as e:
             scan_result = e
     else:
